173ecb8397cb3912dc42de18b11fcc7319887e97
Sunny <wntjs0305@gmail.com>
2022-12-15 00:24:11 +0900

    12. Data Types

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,48 @@
+/*
+let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $funtion = 27;
+
+let person = 'jonas'
+let PI = 3.14515;
+
+let myFirstJob = 'Corder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
