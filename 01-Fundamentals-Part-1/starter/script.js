0195f99e14637744002ab067c4060a849cbe4f06
Sunny <wntjs0305@gmail.com>
2022-12-16 02:43:48 +0900

    18. Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -49,6 +49,7 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
 let age = 30;
 age = 31;
 
@@ -61,3 +62,112 @@ job = 'teacher'
 
 lastName = 'Schmedtmann';
 console.log(lastName);
+*/
+
+/*
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
+/*
+// Coding Challenge #1
+function calculateBMI(mass, height) {
+    let bmi = mass / height ** 2;
+    console.log(bmi);¡
+};
+
+const marksWeight1 = 78;
+const marksHeight1 = 1.69;
+calculateBMI(marksWeight1, marksHeight1);
+const johnWeight1 = 92;
+const johnHeight1 = 1.95;
+calculateBMI(johnWeight1, johnHeight1);
+const marksWeight2 = 95;
+const marksHeight2 = 1.88;
+calculateBMI(marksWeight2, marksHeight2);
+const johnWeight2 = 85;
+const johnHeight2 = 1.76;
+calculateBMI(johnWeight2, johnHeight2);
+*/
+
+/*
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\multiple \n\lines');
+
+console.log(`String
+multiple
+lines`);
+*/
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
